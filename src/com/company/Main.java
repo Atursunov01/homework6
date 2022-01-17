94a2f1b4c450da1ae946dee9907c2c7595231efc
Elvira <akzholtursunov01@gmail.com>
2022-01-17 21:59:32 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        // write your code here
+//        int a = 1;
+//        int summa = 0;
+//        for (int i = a; i <= 10; i++) {
+//            summa = summa + i;
+//            System.out.println(summa);
+//
+//        }
+//    }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("azho");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int s = scanner.nextInt();
+        System.out.println(a * b * s / (double ) 3 );
+
+    }
+}
+
+
